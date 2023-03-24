6197a2f98acc624e1429978a17f993f3cdfec61f
Yaroslav Satsyuk <yaroslav.satsyuk@workjam.com>
2023-03-24 14:22:59 -0400

    Add location manager delegate

--- Sources/tca-core-location-swift-concurrency/LocationManager.swift
+++ Sources/tca-core-location-swift-concurrency/LocationManager.swift
@@ -14,6 +14,7 @@ public struct LocationManager {
         case didEnterRegion(Region)
         case didExitRegion(Region)
         case didFailWithError(Error)
+        case didFinishDeferredUpdatesWithError(Error?)
         case didPauseLocationUpdates
         case didResumeLocationUpdates
         case didStartMonitoring(region: Region)
@@ -94,3 +95,68 @@ public extension LocationManager {
     }
 }
 
+
+private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
+    let continuation: AsyncStream<LocationManager.Action>.Continuation
+    
+    init(continuation: AsyncStream<LocationManager.Action>.Continuation) {
+        self.continuation = continuation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        self.continuation.yield(.didChangeAuthorization(status))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.continuation.yield(.didFailWithError(LocationManager.Error(error)))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.continuation.yield(.didUpdateLocations(locations.map(Location.init(rawValue:))))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?
+    ) {
+        self.continuation.yield(
+            .didFinishDeferredUpdatesWithError(error.map(LocationManager.Error.init))
+        )
+    }
+    
+    func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager) {
+        self.continuation.yield(.didPauseLocationUpdates)
+    }
+    
+    
+    func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
+        self.continuation.yield(.didResumeLocationUpdates)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        self.continuation.yield(.didEnterRegion(Region(rawValue: region)))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        self.continuation.yield(.didExitRegion(Region(rawValue: region)))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion
+    ) {
+        self.continuation.yield(.didDetermineState(state, region: Region(rawValue: region)))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error
+    ) {
+        self.continuation.yield(
+            .monitoringDidFail(
+                region: region.map(Region.init(rawValue:)),
+                error: LocationManager.Error(error)
+            )
+        )
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        self.continuation.yield(.didStartMonitoring(region: Region(rawValue: region)))
+    }
+    
+}
