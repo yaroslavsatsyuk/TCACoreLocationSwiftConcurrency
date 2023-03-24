6d477a51a1e34b7bbe0d5e6a617f794e91e24ae9
Yaroslav Satsyuk <yaroslav.satsyuk@workjam.com>
2023-03-24 13:18:10 -0400

    Import Dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "tca-core-location-swift-concurrency",
+    platforms: [
+       .iOS(.v14),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +15,16 @@ let package = Package(
             targets: ["tca-core-location-swift-concurrency"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "tca-core-location-swift-concurrency",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+            ]),
         .testTarget(
             name: "tca-core-location-swift-concurrencyTests",
             dependencies: ["tca-core-location-swift-concurrency"]),
