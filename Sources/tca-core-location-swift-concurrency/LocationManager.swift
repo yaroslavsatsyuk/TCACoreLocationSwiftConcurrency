0f0854732579f7ef1eee4e816493f2d2e4ff554c
Yaroslav Satsyuk <yaroslav.satsyuk@workjam.com>
2023-03-24 14:14:14 -0400

    Add properties for LocationManager

--- Sources/tca-core-location-swift-concurrency/LocationManager.swift
+++ Sources/tca-core-location-swift-concurrency/LocationManager.swift
@@ -7,7 +7,7 @@
 
 import CoreLocation
 
-public struct LocationMananger {
+public struct LocationManager {
     public enum Action: Equatable {
         case didChangeAuthorization(CLAuthorizationStatus)
         case didDetermineState(CLRegionState, region: Region)
@@ -22,11 +22,75 @@ public struct LocationMananger {
     }
     
     public struct Error: Swift.Error, Equatable {
-      public let error: NSError
-
-      public init(_ error: Swift.Error) {
-        self.error = error as NSError
-      }
+        public let error: NSError
+        
+        public init(_ error: Swift.Error) {
+            self.error = error as NSError
+        }
     }
     
+    public var accuracyAuthorization: () -> AccuracyAuthorization?
+    public var authorizationStatus: () -> CLAuthorizationStatus
+    public var delegate: @Sendable () -> AsyncStream<Action>
+    public var location: () -> Location?
+    public var locationServicesEnabled: () -> Bool
+    public var requestAlwaysAuthorization: @Sendable () async -> Void
+    public var requestWhenInUseAuthorization: @Sendable () async -> Void
+    public var requestTemporaryFullAccuracyAuthorization: @Sendable (String) async throws -> Void
+    public var requestLocation: @Sendable () async -> Void
+    public var startUpdatingLocation: @Sendable () async -> Void
+    public var stopUpdatingLocation: @Sendable () async -> Void
+    public var monitoredRegions: () -> Set<Region>
+    public var stopMonitoringForRegion: @Sendable (Region) async -> Void
+    public var set: @Sendable (LocationManager.Properties) async -> Void
+    
+    @Sendable public func set(
+        activityType: CLActivityType? = nil,
+        allowsBackgroundLocationUpdates: Bool? = nil,
+        desiredAccuracy: CLLocationAccuracy? = nil,
+        distanceFilter: CLLocationDistance? = nil,
+        headingFilter: CLLocationDegrees? = nil,
+        headingOrientation: CLDeviceOrientation? = nil,
+        pausesLocationUpdatesAutomatically: Bool? = nil,
+        showsBackgroundLocationIndicator: Bool? = nil
+    ) async {
+        await self.set(
+            Properties(
+                activityType: activityType,
+                allowsBackgroundLocationUpdates: allowsBackgroundLocationUpdates,
+                desiredAccuracy: desiredAccuracy,
+                distanceFilter: distanceFilter,
+                pausesLocationUpdatesAutomatically: pausesLocationUpdatesAutomatically,
+                showsBackgroundLocationIndicator: showsBackgroundLocationIndicator
+            )
+        )
+    }
 }
+
+public extension LocationManager {
+    struct Properties: Equatable {
+        var activityType: CLActivityType? = nil
+        var allowsBackgroundLocationUpdates: Bool? = nil
+        var desiredAccuracy: CLLocationAccuracy? = nil
+        var distanceFilter: CLLocationDistance? = nil
+        var pausesLocationUpdatesAutomatically: Bool? = nil
+        var showsBackgroundLocationIndicator: Bool? = nil
+        
+        public init(
+            activityType: CLActivityType? = nil,
+            allowsBackgroundLocationUpdates: Bool? = nil,
+            desiredAccuracy: CLLocationAccuracy? = nil,
+            distanceFilter: CLLocationDistance? = nil,
+            pausesLocationUpdatesAutomatically: Bool? = nil,
+            showsBackgroundLocationIndicator: Bool? = nil
+        ) {
+            self.activityType = activityType
+            self.allowsBackgroundLocationUpdates = allowsBackgroundLocationUpdates
+            self.desiredAccuracy = desiredAccuracy
+            self.distanceFilter = distanceFilter
+            self.pausesLocationUpdatesAutomatically = pausesLocationUpdatesAutomatically
+            self.showsBackgroundLocationIndicator = showsBackgroundLocationIndicator
+        }
+    }
+}
+
